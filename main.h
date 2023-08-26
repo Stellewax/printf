b558646edc76157dfb42b54ae27a95b39abfadc3
stella <awaziestella@gmail.com>
2023-08-26 09:47:29 -0700

    char_string first commit

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int print_numbers(int n);
+int _putchar(char z);
+int _printf(const char *format, ...);
+int put_str(char *string);
+int put_int(int integer);
+int get_int(int num);
+
+#endif
